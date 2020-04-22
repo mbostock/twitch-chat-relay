82bcd3b1c1044bff3cc4dcb892b1e059f0ae99e3
Mike Bostock <mbostock@gmail.com>
2020-04-21 21:28:26 -0700

    Relay Twitch chat.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require("http");
-const ws = require("ws");
+const url = require("url");
+const WebSocket = require("ws");
+const EventEmitter = require("events");
 
 const port = process.env.PORT || 5000;
 
@@ -9,11 +11,40 @@ const server = http.createServer((req, res) => {
   res.end("404");
 });
 
-const socketServer = new ws.Server({server});
+let socket;
+let timeout;
+let delay = 250;
+let emitter = new EventEmitter();
 
-socketServer.on("connection", connection => {
-  connection.on("message", message => console.log("received: %s", message));
-  connection.send("hello");
+(function open() {
+  socket = new WebSocket("wss://irc-ws.chat.twitch.tv:443");
+  socket.onopen = () => {
+    delay = 250;
+    socket.send(`PASS ${process.env.TWITCH_OAUTH_TOKEN}`);
+    socket.send(`NICK heloworld`);
+    socket.send(`JOIN #nl_kripp`);
+  };
+  socket.onclose = () => {
+    delay *= 2;
+    timeout = setTimeout(open, delay + Math.random() * delay);
+  };
+  socket.onmessage = event => {
+    emitter.emit("message", event);
+  };
+})();
+
+process.on("SIGTERM", () => {
+  socket.onclose = null;
+  socket.close();
+  process.exit(0);
+});
+
+const socketServer = new WebSocket.Server({server});
+
+socketServer.on("connection", (socket, request) => {
+  const message = event => socket.send(event.data);
+  emitter.addListener("message", message);
+  socket.on("close", () => emitter.removeListener("message", message));
 });
 
 server.listen(port, () => {
