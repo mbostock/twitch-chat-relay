9c3af0733176c3394ba84f76907613812a55d529
Mike Bostock <mbostock@gmail.com>
2020-04-21 21:01:36 -0700

    Listen on 0.0.0.0.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require("http");
 
-const hostname = "127.0.0.1";
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
@@ -9,6 +8,6 @@ const server = http.createServer((req, res) => {
   res.end("Hello World");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at :${port}`);
 });
