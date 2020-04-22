fb437d22b5b0e4abacd1889f052c7d1c19b4a027
Mike Bostock <mbostock@gmail.com>
2020-04-21 21:07:27 -0700

    ws

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const http = require("http");
+const ws = require("ws");
 
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+  res.statusCode = 404;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.end("404");
+});
+
+const socketServer = new ws.Server({server});
+
+socketServer.on("connection", connection => {
+  connection.on("message", message => console.log("received: %s", message));
+  connection.send("hello");
 });
 
 server.listen(port, () => {
